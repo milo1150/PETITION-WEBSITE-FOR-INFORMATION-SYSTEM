c3d3e96be1bffd106214d36f51f32d2d480af78a
miscit <oosamuoo02@gmail.com>
2020-08-09 16:07:23 +0700

    graph update month dynamic

--- application/models/Admin_report_model.php
+++ application/models/Admin_report_model.php
@@ -3,7 +3,7 @@ Class Admin_report_model extends CI_Model{
 
     public function flexReport($post){
         // print_r($post);
-        if($post['diff'] <= 62){
+        if($post['format'] == "date"){
             $username = $post['username'];
             $startDate = strtotime($post['start']);
             $diff = $post['diff'];
@@ -25,6 +25,31 @@ Class Admin_report_model extends CI_Model{
                 $data[$x][2] = $fix_close+$item_close+$email_close+$finger_close+$itemotp_close; 
             }
             return $data;
+        }
+        if($post['format'] == 'month'){
+            $username = $post['username'];
+            $diff = $post['diff'];
+            $mArr = $post['mArr'];  
+
+            $mRangeData = array();                                      
+                for($x=0;$x<=$diff;$x++){                   
+                    $start = $mArr[$x][0];
+                    $end = $mArr[$x][1];
+                    /*---------------------- ACCEPT ORDER --------------------*/
+                    $fix_accept = count($this->db->select('*')->from('request_fix')->where(" admin_accept_name = '$username' AND admin_accept_date BETWEEN '$start' AND '$end' ")->get()->result());
+                    $item_accept = count($this->db->select('*')->from('request_item')->where(" admin_accept_name = '$username' AND admin_accept_date BETWEEN '$start' AND '$end' ")->get()->result());                       
+                    /*---------------------- CLOSE ORDER --------------------*/
+                    $fix_close = count($this->db->select('*')->from('request_fix')->where(" admin_close_name = '$username' AND admin_close_date BETWEEN '$start' AND '$end' ")->get()->result());
+                    $item_close = count($this->db->select('*')->from('request_item')->where(" admin_close_name = '$username' AND admin_close_date BETWEEN '$start' AND '$end' ")->get()->result());
+                    $email_close = count($this->db->select('*')->from('request_email')->where(" admin_close_name = '$username' AND admin_close_date BETWEEN '$start' AND '$end' ")->get()->result());
+                    $finger_close = count($this->db->select('*')->from('request_finger')->where(" admin_close_name = '$username' AND admin_close_date BETWEEN '$start' AND '$end' ")->get()->result());
+                    $itemotp_close = count($this->db->select('*')->from('request_itemotp')->where(" admin_close_name = '$username' AND admin_close_date BETWEEN '$start' AND '$end' ")->get()->result());                               
+                    $mRangeData[$x][0] = $start;
+                    $mRangeData[$x][1] = $fix_accept+$item_accept;          
+                    $mRangeData[$x][2] = $fix_close+$item_close+$email_close+$finger_close+$itemotp_close;                                  
+                } 
+            // print_r($mRangeData);
+            return $mRangeData;
         }
     }
 
--- miscit-js/admin_report.js
+++ miscit-js/admin_report.js
@@ -17,6 +17,11 @@ const username = document.getElementById('uName').attributes[1].value // admin n
 $(function() {    
     var start = moment().subtract(29, 'days');
     var end = moment();
+    // var mstart = start.month()
+    // console.log(mstart)
+
+
+
     $('input[name="daterange"]').daterangepicker({
         startDate: start,
         endDate: end,
@@ -27,24 +32,64 @@ $(function() {
     }, function(start, end, label) {        
         const newStart = start.format('YYYY-MM-DD');
         const newEnd = end.format('YYYY-MM-DD');
-        const diff = end.diff(start,'days');
-        axios.post('./flexReport',{
-            username: username,
-            start: newStart,
-            end: newEnd,
-            diff: diff,
-        })
-        .then((res) => {
-            // console.log(res.data)
-            overallChange(res,diff)
-        })
+        const diff = end.diff(start,'days');       
+
+        // ------ Month Array For backendLoop -------
+        const mStart = start.format('YYYY-MM');
+        const mEnd = end.format('YYYY-MM');
+        const mDiff = end.diff(start,'month');
+
+        const getMrange = (start) => {                     
+            const mArr = Array();       
+            for(let i=0;i<=mDiff;i++){
+                let mS = start.startOf('month').format('YYYY-MM-DD')
+                let mE = start.endOf('month').format('YYYY-MM-DD')
+                mArr[i] = [ mS , mE ]
+                start.add(1,'M')
+            }
+            return mArr
+            // console.log(mArr)
+        }
+
+        if(diff <= 62){
+            axios.post('./flexReport',{
+                username: username,
+                start: newStart,
+                end: newEnd,
+                diff: diff,
+                format: 'date'
+            })
+            .then((res) => {
+                // console.log(res.data)
+                overallChange(res,diff)
+            })
+        }
+        if(diff > 62){
+            let data = getMrange(start)
+            axios.post('./flexReport',{
+                username: username,
+                mArr: data,
+                diff: mDiff,
+                format: 'month'
+            })
+            .then((res) => {
+                // console.log(res.data)
+                overallChange(res,diff)
+            })
+        }
+        
+        
+
+        
+        
+        
         // a(newStart)
         // console.log(newStart)
         // console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
 });
 const overallChange = (res,diff) => {
-    console.log(diff)
+    // console.log(diff)
     // ------- 2 Months range ---------
     if(diff <= 62){ 
         const data = res.data;
@@ -137,7 +182,95 @@ const overallChange = (res,diff) => {
         window.load = new Chart(document.getElementById('data_overall').getContext('2d'),overall);
     }  
     if(diff > 62){
-        console.log(1)
+        const data = res.data;
+        var acceptData = Array();
+        var closeData = Array();
+        var acceptDataColor = Array();
+        var closeDataColor = Array();
+        console.log(data)
+        
+        for(let i in data){
+            // ------ accept data ---------
+            let a = {
+                x:data[i][0],
+                y:data[i][1]
+            }        
+            acceptData.push(a)        
+            acceptDataColor.push('rgba(255,0,0,1)')
+            // ------ close data ------
+            let b = {
+                x:data[i][0],
+                y:data[i][2]
+            }
+            closeData.push(b)
+            closeDataColor.push('rgba(50,205,50,1)')
+        }
+        const min = data[0][0]
+        const max = data[data.length-1][0]
+
+        var overall = {
+            type: 'line',
+            data: {
+                datasets: [{
+                        label: 'รับงาน',                
+                        data: acceptData,                
+                        pointBackgroundColor: acceptDataColor,
+                        backgroundColor: [
+                            'rgba(255,0,0,0.1)',
+                        ],
+                        borderColor: [
+                            'rgba(255,0,0,0.4)',
+                        ],
+                        borderWidth: 3,
+                        pointHoverBorderWidth: 1,
+                        pointHoverRadius: 3,
+                        pointRadius: 3,
+                    },
+                    {
+                        label: 'ปิดงาน',                
+                        data: closeData,                
+                        pointBackgroundColor: closeDataColor,
+                        backgroundColor: [
+                            'rgba(50,205,50,0.1)',
+                        ],
+                        borderColor: [
+                            'rgba(50,205,50,0.4)',
+                        ],
+                        borderWidth: 3,
+                        pointHoverBorderWidth: 1,
+                        pointHoverRadius: 3,
+                        pointRadius: 3,
+                    },           
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    xAxes: [{                
+                        type: 'time',                           
+                        time:{    
+                            unit: 'month',
+                            format:'YYYY MM DD',               
+                            displayFormats:{
+                                'month': 'MMM YY',
+                            },               
+                            tooltipFormat:'MMM YYYY',  
+                            min:min,    
+                            max:max,
+                            stepSize:1, 
+                        },
+                    }
+                    ],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            precision:0,
+                        }
+                    }],
+                },
+            }
+        }
+        window.load = new Chart(document.getElementById('data_overall').getContext('2d'),overall);
     }  
 }
 
